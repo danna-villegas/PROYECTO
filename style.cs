4ff686005085e98a42ffd6bf22c67d714560f739
Danna Ferreira <147215926+danna-villegas@users.noreply.github.com>
2023-11-27 21:34:42 -0500

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,107 @@
+body{
+    margin: 0px;
+    background-color: #add8e6 ;
+    font-family: 'Nerko One', cursive;
+    display: flex;
+   
+}
+
+h1{
+  color: rgb(179, 71, 71);  
+  font-size: 100px;
+}
+
+
+.contenedor{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+.card{
+
+    margin: 5px;
+    border: 1px solid;
+    color: #c1c2ca;
+    padding: 16px;
+    border-radius: 20px;
+    display: grid;
+    width: 200px;
+    height: 10px;
+    font-size: 15px;
+}
+.orange{
+    display: flex;
+}
+.card-image-top{
+    width: 150px;
+    
+}
+
+/* carrusel*/
+
+.carousel-item{
+    width: 150%;
+    height: 100%;
+}
+
+.carousel-item :active{
+    width: 150%;
+    height: 100%;
+}
+
+.carrusel{
+    display: block; 
+}
+
+
+
+/*footer{
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    background-color: #f6fa0a;
+    height: 100px;
+    padding: 20px 50px;
+    flex-direction: column;
+}
+
+footer .social-icons,
+footer .menu {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px 0;
+    flex-wrap: wrap;
+
+}
+
+footer .social-icons li,
+footer .menu li{
+    list-style: none;
+} */
+
+
+/*inicio logo*/
+ 
+.inicio{
+
+    border: rgb(23, 23, 240) solid 2px;
+    padding: 10px ;
+    width: auto;
+    height: auto;
+    display: block;
+    margin: 100px;
+
+}
+
+.card {
+    border: rgb(36, 69, 235) solid 2px;
+    padding: 50px ;
+    width: auto;
+    height: 10px;
+    display: block;
+    margin: 90px;
+}
